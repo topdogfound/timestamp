4d1636ce8e9c5fbfddda908441fb931512041e53
topdogfound <topdogfound@gmail.com>
2025-08-03 13:12:02 +0530

    test

--- index.js
+++ index.js
@@ -2,44 +2,50 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
-app.get('/api/:date', (req, res)=>{
-  let date = new Date(req.params.date);
-  if(isNaN(date.getTime())){
-    res.json({
-      error : "Invalid Date"
-    })
+app.get('/api/:date?', (req, res) => {
+  let reqDate = req.params.date;
+
+  // If no date provided, use current date
+  let date = reqDate ? new Date(reqDate) : new Date();
+
+  console.log(date);
+  // If date is invalid AND input is a number string, try to parse as timestamp
+  if (isNaN(date.getTime()) && /^\d+$/.test(reqDate)) {
+    date = new Date(parseInt(reqDate));
+  }
+
+  if (isNaN(date.getTime())) {
+    return res.json({ error: 'Invalid Date' });
   }
 
   res.json({
-    unix : date.getTime(),
-    utc : date.toUTCString()
-  })
-})
-
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
+});
 
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
